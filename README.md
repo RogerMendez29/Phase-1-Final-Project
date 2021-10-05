98fa046261b922747b136e1b37651442916b922d
Roger Mendez <roger61087@gmail.com>
2021-10-05 15:17:54 -0400

    Finalized Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Phase-1-Final-Project
+
+This Projects was mainly conducted with javascript some basic css and minimal html. It uses an api to retrive random activities and creates a json server to store that data. The user is able to click an icon and retrive a random activity, they are then able to add an activity to a favorites list and with the use of the json server the users changes are able to persist.
+
+API Used: https://www.boredapi.com/api/activity
+
+Photos used: https://www.pexels.com/photo/hot-air-ballons-in-the-sky-2325446/
+Retrived from: PEXELS.COM
+Photographer:Francesco Ungaro
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// added text anmation to the title of project
+
 const text = document.querySelector("#title");
 const stripText = text.textContent;
 const splitText = stripText.split("");
@@ -16,20 +18,22 @@ function onTick() {
   count++;
   if (count === splitText.length) {
     complete();
-    return;
   }
 }
 function complete() {
   clearInterval(timer);
-  timer = null;
 }
 
+// grabbed each letter individually and turned it into an array in order to iterate through
 const titleLetters = document.querySelectorAll("span");
 let newLetters = Array.from(titleLetters);
 
 newLetters.forEach((letter) => {
   letter.addEventListener("click", getData);
 });
+
+// created a function that grabbed data from the api and turned it into a json().
+// created an activity that retrived the wanted values from the APi
 
 function getData() {
   document.querySelector(".loading").style.display = "block";
@@ -41,7 +45,10 @@ function getData() {
         Type: act.type,
         Participants: act.participants,
       };
+
       document.querySelector(".loading").style.display = "none";
+      // a response was retrived after PostAct() completed its fetch.
+      // this was done to have an ID specific to an activity
       postAct(activity)
         .then((res) => res.json())
         .then((theAct) => addToList(theAct, "activities"));
@@ -57,6 +64,59 @@ function postAct(activity) {
     },
     body: JSON.stringify(activity),
   });
+}
+
+function addToList(activity, list) {
+  let activityList = document.querySelector(`#${list}`);
+  let listItem = document.createElement("li");
+  let activityInfo = document.createElement("ul");
+  // used an if statement to not append the id to the DOM
+  for (const key in activity) {
+    if (key !== "id") {
+      let activityContent = document.createElement("li");
+      activityContent.innerHTML = `${key}: ${activity[key]}`;
+      activityInfo.append(activityContent);
+    }
+  }
+
+  listItem.append(activityInfo);
+  activityList.append(listItem);
+
+  const btn = document.createElement("button");
+
+  if (list === "activities") {
+    btn.textContent = "Add to Favorites";
+    btn.addEventListener("click", (ev) => {
+      addToList(activity, "favorites");
+      deleteAct(activity.id);
+      // delete proper fixed the duplicate id issue
+      delete activity.id;
+      postFavorites(activity);
+      listItem.remove();
+      ev.target.remove();
+      deleteBtn.remove();
+      // console.log(ev);
+    });
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", (ev) => {
+      deleteAct(activity.id);
+      listItem.remove();
+      ev.target.remove();
+      btn.remove();
+      deleteBtn.remove();
+    });
+    activityList.append(deleteBtn);
+  } else if (list === "favorites") {
+    btn.textContent = "Delete";
+    btn.addEventListener("click", () => {
+      deleteFavorites(activity.id);
+      listItem.remove();
+      btn.remove();
+    });
+  }
+
+  activityList.append(btn);
 }
 
 function fetchActs() {
@@ -103,56 +163,3 @@ function fetchFavorites() {
     .then((acts) => acts.forEach((element) => addToList(element, "favorites")));
 }
 fetchFavorites();
-
-function addToList(activity, list) {
-  let activityList = document.querySelector(`#${list}`);
-  let listItem = document.createElement("li");
-  let activityInfo = document.createElement("ul");
-
-  for (const key in activity) {
-    if (key !== "id") {
-      let activityContent = document.createElement("li");
-      activityContent.innerHTML = `${key}: ${activity[key]}`;
-      activityInfo.append(activityContent);
-    }
-  }
-
-  listItem.append(activityInfo);
-  activityList.append(listItem);
-
-  const btn = document.createElement("button");
-
-  if (list === "activities") {
-    const deleteBtn = document.createElement("button");
-
-    btn.textContent = "Add to Favorites";
-    btn.addEventListener("click", (ev) => {
-      addToList(activity, "favorites");
-      deleteAct(activity.id);
-      delete activity.id;
-      postFavorites(activity);
-      listItem.remove();
-      ev.target.remove();
-      deleteBtn.remove();
-    });
-
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener("click", (ev) => {
-      deleteAct(activity.id);
-      listItem.remove();
-      ev.target.remove();
-      btn.remove();
-      deleteBtn.remove();
-    });
-    activityList.append(deleteBtn);
-  } else if (list === "favorites") {
-    btn.textContent = "Delete";
-    btn.addEventListener("click", () => {
-      deleteFavorites(activity.id);
-      listItem.remove();
-      btn.remove();
-    });
-  }
-
-  activityList.append(btn);
-}
